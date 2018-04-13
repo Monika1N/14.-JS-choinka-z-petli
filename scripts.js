bda47e9ca0049bf15cb533a644c3ab753de64fe6
Monika N <monia.niemczyk1@gmail.com>
2018-04-13 18:54:40 +0200

    Do a crooked tree

---
+++ scripts.js
@@ -0,0 +1,12 @@
+function drawTree(height) {
+	for (var i = 0 ; i <= height ; i++) {
+		var star = "";
+		for (var j = 0 ; j < i ; j++ ) {
+		var star = star += "*";
+		}
+		console.log(star);
+	}
+};
+
+drawTree(5);
+
