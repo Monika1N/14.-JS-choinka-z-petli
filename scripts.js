a29518732aab6f1d5d5f855d9a1d554583277f70
Monika N <monia.niemczyk1@gmail.com>
2018-04-14 11:21:44 +0200

    Exercise

--- scripts.js
+++ scripts.js
@@ -1,12 +1,13 @@
 //crooked tree
 
 function drawTree(height) {
-	for (var i = 0 ; i <= height ; i++) {
+	for (var i = 1 ; i <= height ; i++) {
 		var star = "";
-		for (var j = 0 ; j < i ; j++) {
+		for (var j = 1 ; j <= i ; j++) {
 		star = star += "*";
 		}
 		console.log(star);
 	}
 };
 
+drawTree(5);
