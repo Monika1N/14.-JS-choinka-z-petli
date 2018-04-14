419cfee216b46068c899086089e85d741f624f44
Monika N <monia.niemczyk1@gmail.com>
2018-04-14 11:15:14 +0200

    Exercise

--- scripts.js
+++ scripts.js
@@ -3,14 +3,10 @@
 function drawTree(height) {
 	for (var i = 0 ; i <= height ; i++) {
 		var star = "";
-		for (var j = 0 ; j < i ; j++ ) {
-		var star = star += "*";
+		for (var j = 0 ; j < i ; j++) {
+		star = star += "*";
 		}
 		console.log(star);
 	}
 };
-
-drawTree(5);
-
-//straight tree
 
